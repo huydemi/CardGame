74bbac953332c7d8935f6ee7554e79f7e66dea2d
Dang Quoc Huy <huysamdua@yahoo.com>
2017-08-31 23:47:50 +0700

    The Card Class

--- CardGameV2/Card.swift
+++ CardGameV2/Card.swift
@@ -22,6 +22,35 @@
 
 import SpriteKit
 
-class Card : SKSpriteNode {
-  
+enum CardType :Int {
+  case wolf,
+  bear,
+  dragon
 }
+
+class Card : SKSpriteNode {
+  let cardType :CardType
+  let frontTexture :SKTexture
+  let backTexture :SKTexture
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("NSCoding not supported")
+  }
+  
+  init(cardType: CardType) {
+    self.cardType = cardType
+    backTexture = SKTexture(imageNamed: "card_back")
+    
+    switch cardType {
+    case .wolf:
+      frontTexture = SKTexture(imageNamed: "card_creature_wolf")
+    case .bear:
+      frontTexture = SKTexture(imageNamed: "card_creature_bear")
+    case .dragon:
+      frontTexture = SKTexture(imageNamed: "card_creature_dragon")
+    }
+    
+    super.init(texture: frontTexture, color: .clear, size: frontTexture.size())
+  }
+}
+
--- CardGameV2/GameScene.swift
+++ CardGameV2/GameScene.swift
@@ -23,14 +23,20 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+  
   override func didMove(to view: SKView) {
     let bg = SKSpriteNode(imageNamed: "bg_blank")
     bg.anchorPoint = CGPoint.zero
     bg.position = CGPoint.zero
     addChild(bg)
     
+    let wolf = Card(cardType: .wolf)
+    wolf.position = CGPoint(x: 100, y: 200)
+    addChild(wolf)
     
-    }
-
+    let bear = Card(cardType: .bear)
+    bear.position = CGPoint(x: 300, y: 200)
+    addChild(bear)
+  }
+  
 }
